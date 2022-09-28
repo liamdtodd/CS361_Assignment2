82434a00142c6f3c73c7f61cd7a248fa8d7f4a51
Liam Todd <toddl@flip1.engr.oregonstate.edu>
2022-09-27 19:53:23 -0700

    implemented i/o with user, need to implement file writing

---
+++ user.cpp
@@ -0,0 +1,24 @@
+/**************************
+ * Liam Todd
+ * CS 361 - Assignment2
+ * This code file is the user interface for the microservice, will prompt the user, and write to prng-service.txt
+ * ***********************/
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main () {
+	int ans;
+	cout << "Would you like to start the microservice? 1-yes, 0-no" << endl;
+	cin >> ans;
+
+	if (ans == 1) {
+	//implement writing to prng-service.txt file the word 'run'
+	}
+
+	return 0;
+}
