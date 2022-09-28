f11abc9226c8d74cd5b31965385b52cce45a2837
Liam Todd <toddl@flip2.engr.oregonstate.edu>
2022-09-28 13:18:28 -0700

    file i/o implemented

--- prng-service.cpp
+++ prng-service.cpp
@@ -8,17 +8,40 @@
 #include <fstream>
 #include <iostream>
 #include <ctime>
+#include <cstring>
 
 using namespace std;
 
 int main() {
-	//read 'run' from prng-service.txt
-
 	srand(time(NULL));
-	
-	int rand_num = rand() % 5 + 1;
-	
-	//write rand_num to image-service.txt
 
+	fstream readfile;
+	fstream writefile;
+	int rand_num;
+
+	if (readfile.is_open())
+		readfile.close();
+
+	readfile.open("prng-service.txt", ios::in);
+	string ident = "run";
+	string text;
+
+	getline(readfile, text);
+
+	if (text == ident) 
+		rand_num = rand() % 5 + 1;
+	readfile.close();
+	
+	readfile.open("prng-service.txt", ios::out | ios::trunc);
+	readfile.close();
+
+	if (writefile.is_open())
+		writefile.close();
+
+	writefile.open("image-service.txt", ios::out);
+	writefile << rand_num << endl;
+	writefile.close();
+	
+	
 	return 0;
 }
