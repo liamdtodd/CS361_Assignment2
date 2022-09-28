d4197c08cc7db214873ea10becad40a226876ef9
Liam Todd <toddl@flip1.engr.oregonstate.edu>
2022-09-27 19:38:47 -0700

    random number is generated, still need file i/o

--- prng-service.cpp
+++ prng-service.cpp
@@ -1,1 +1,24 @@
+/*******************************
+ * Liam Todd
+ * CS 361 - Assignment 2
+ * This code file will read from a text file and generate a random number that will be written to a seperate text file
+ * *****************************/
 
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	//read 'run' from prng-service.txt
+
+	srand(time(NULL));
+	
+	int rand_num = rand() % 5 + 1;
+	
+	//write rand_num to image-service.txt
+
+	return 0;
+}
