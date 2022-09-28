e62e92b6cd5fb940c1c7179489c9f5613e146283
Liam Todd <toddl@flip1.engr.oregonstate.edu>
2022-09-27 19:43:44 -0700

    mod'ing random number if need be. need to implement file reading, producing image

--- image-service.cpp
+++ image-service.cpp
@@ -1,1 +1,23 @@
+/***************************
+ * Liam Todd
+ * CS 361 - Assignment 2
+ * This code file will read a random number from image-service.txt and use that random number to access the associated image
+ * *************************/
 
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	//read from image-service.txt for random number
+
+	if (rand_num > 5) 
+		rand_num = rand_num % 5;
+
+	//implement using rand_num to display associated image 
+	
+	return 0;
+}
