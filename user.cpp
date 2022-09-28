588b33f72056344116d8d2939abc251e8bf87854
Liam Todd <toddl@flip2.engr.oregonstate.edu>
2022-09-28 13:18:10 -0700

    file output implemented

--- user.cpp
+++ user.cpp
@@ -13,12 +13,19 @@ using namespace std;
 
 int main () {
 	int ans;
+	fstream file;
+	
 	cout << "Would you like to start the microservice? 1-yes, 0-no" << endl;
 	cin >> ans;
 
 	if (ans == 1) {
 	//implement writing to prng-service.txt file the word 'run'
+		if (file.is_open())
+			file.close();
+		file.open("prng-service.txt", ios::out);
+		file << "run" << endl;
 	}
+	file.close();
 
 	return 0;
 }
