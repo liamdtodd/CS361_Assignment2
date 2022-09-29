be04fdd41c614150e722f977566ccf03d36af653
Liam Todd <toddl@flip2.engr.oregonstate.edu>
2022-09-29 14:21:05 -0700

    fixed file i/o, time added

--- user.cpp
+++ user.cpp
@@ -8,11 +8,15 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <chrono>
+#include <thread>
 
 using namespace std;
+using namespace chrono;
+using namespace this_thread;
 
 int main () {
-	int ans;
+	int ans, rand_num;
 	fstream file;
 	
 	cout << "Would you like to start the microservice? 1-yes, 0-no" << endl;
@@ -25,6 +29,18 @@ int main () {
 		file.open("prng-service.txt", ios::out);
 		file << "run" << endl;
 	}
+	file.close();
+
+	sleep_for(seconds(5));
+
+	file.open("prng-service.txt", ios::in);
+	file >> rand_num;
+	file.close();
+
+	sleep_for(seconds(5));
+
+	file.open("image-service.txt", ios::out);
+	file << rand_num << endl;
 	file.close();
 
 	return 0;
