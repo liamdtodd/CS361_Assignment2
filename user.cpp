06b3166e8c3464da3a161a1eccd3ab0038f0870c
Liam Todd <toddl@flip1.engr.oregonstate.edu>
2022-09-29 16:41:28 -0700

    string_to_int, bug fixes, file i/o

--- user.cpp
+++ user.cpp
@@ -15,33 +15,61 @@ using namespace std;
 using namespace chrono;
 using namespace this_thread;
 
+int string_to_int(string text) {
+	int num, temp;
+	char letter;
+	num = 0;	
+
+	for (int x = 0; x < text.length(); x++) {
+		letter = text[x];
+		temp = letter - 48;
+		if (x == 0) 
+			num += temp;
+		else {
+			num *= 10;
+			num += temp;
+		}
+	}
+	return num;
+}	
+
 int main () {
 	int ans, rand_num;
-	fstream file;
-	
+	string rand, path;
+	ifstream readfile;
+	ofstream writefile;
+	ofstream writefile2;	
+
 	cout << "Would you like to start the microservice? 1-yes, 0-no" << endl;
 	cin >> ans;
 
 	if (ans == 1) {
 	//implement writing to prng-service.txt file the word 'run'
-		if (file.is_open())
-			file.close();
-		file.open("prng-service.txt", ios::out);
-		file << "run" << endl;
+		if (writefile.is_open())
+			writefile.close();
+		writefile.open("prng-service.txt", ios::out);
+		writefile << "run" << endl;
 	}
-	file.close();
+	writefile.close();
 
 	sleep_for(seconds(5));
 
-	file.open("prng-service.txt", ios::in);
-	file >> rand_num;
-	file.close();
+	readfile.open("prng-service.txt", ios::in);
+	getline(readfile, rand);
+	rand_num = string_to_int(rand);
+	readfile.close();
 
 	sleep_for(seconds(5));
 
-	file.open("image-service.txt", ios::out);
-	file << rand_num << endl;
-	file.close();
+	writefile2.open("image-service.txt", ios::out);
+	writefile2 << rand_num << endl;
+	writefile2.close();
+
+	sleep_for(seconds(3));
+	readfile.open("image-service.txt", ios::in);
+	getline(readfile, path);
+	cout << path << endl;
+	readfile.close();
 
 	return 0;
 }
