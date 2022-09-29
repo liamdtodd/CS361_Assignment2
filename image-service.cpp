23cfc4192c3d1e1cb949bbde07d0369445ead4ee
Liam Todd <toddl@flip1.engr.oregonstate.edu>
2022-09-29 16:41:50 -0700

    file i/o, string_to_int, image path

--- image-service.cpp
+++ image-service.cpp
@@ -15,23 +15,60 @@ using namespace std;
 using namespace chrono;
 using namespace this_thread;
 
+int string_to_int(string text) {
+	int num, temp;
+	char letter;
+	num = 0;
+	
+	for (int x = 0; x < text.length(); x++) {
+		letter = text[x];
+		temp = letter - 48;
+		if (x == 0)
+			num += temp;
+		else {
+			num *= 10;
+			num += temp;
+		}
+	}
+	
+	return num;
+}
+
 int main() {
-	sleep_for(seconds(10));
+	sleep_for(seconds(6));
 
 	int rand_num;
+	string rand;
 	fstream readfile;
+	fstream writefile;
 
 	//read from image-service.txt for random number
 	if (readfile.is_open())
 		readfile.close();
 
 	readfile.open("image-service.txt", ios::in);
-	readfile >> rand_num;
+	getline(readfile, rand);
+	rand_num = string_to_int(rand);
+	readfile.close();
+	cout << rand_num << endl;
 
 	if (rand_num > 5) 
 		rand_num = rand_num % 5;
 
+	writefile.open("image-service.txt", ios::out | ios::trunc);
+	writefile.close();
+
+	writefile.open("image-service.txt", ios::out);
+
+	for (int x = 0; x < 5; x++) {
+		if (x == rand_num) 
+			writefile << "./images/image" << rand_num << ".jpg" << endl;
+	}
+	
+	writefile.close();
 	//implement using rand_num to display associated image 
+
+	
 	
 	return 0;
 }
